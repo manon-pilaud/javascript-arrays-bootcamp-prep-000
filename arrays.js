222b070dee4fc23bb204fe66973cb9122823034e
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-04 20:50:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,3 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 var addElementToBeginningOfArray = ["foo",...chocolateBars];
-
-var destructivelyAddElementToBeginningOfArray = chocolateBars.unshift('foo');
